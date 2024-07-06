cdd3ace790e5d25216cfcc373bba9bc4c60042a7
utkarsh20043 <132839521+utkarsh20043@users.noreply.github.com>
2024-07-06 15:55:50 +0530

    Create DH_draw.m

---
+++ Manipulator/functions/DH_draw.m
@@ -0,0 +1,23 @@
+% This is used to draw lines in a 3D space
+
+function [] = DH_draw(X, Y, Z)
+
+[~, c(1)] = size(X);
+[~, c(2)] = size(Y);
+[~, c(3)] = size(Z);
+
+if (c(1) ~= c(2) || c(1) ~= c(3))
+    error('Invalid arguments')
+    
+end
+
+if (c(1) < 2)
+    error('The input coordinate arrays should have atleast 2 entries')
+end
+
+for i = 1:(c(1)-1)
+    plot3([X(i), X(i+1)], [Y(i), Y(i+1)], [Z(i), Z(i+1)]);
+    hold on
+end
+
+end
